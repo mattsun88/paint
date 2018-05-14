efd0028abd83bed4f4739dad330d8125e71150bd
student <student@student-HP>
2018-05-14 14:50:53 +0900

    スケルトンウィンドウにメニューを付けただけ。中身の処理は空
    Signed-off-by: student <student@student-HP>

---
+++ test/src/test/test2.java
@@ -0,0 +1,38 @@
+package test;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class test2 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+ Scene scene = new Scene(root, 300, 250);
+
+        String value = "Hello World!!!!";
+		primaryStage.setTitle(value);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ public static void main(String[] args) {
+        launch(args);
+    }
+}
