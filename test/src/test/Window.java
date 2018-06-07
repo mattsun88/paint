af0133f021c312bf02132de80aedefaba7559ac4
student <student@student-HP>
2018-06-07 17:12:27 +0900

    まあ本体出さないからこれでレポート書いてもいいっちゃいい

--- test/src/test/Window.java
+++ test/src/test/Window.java
@@ -2,6 +2,7 @@ package test;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -112,22 +113,22 @@ public class Window extends Application {
         	else if(mode==2) {
         		setXY(event);
         	}
-        	else {
+        	else if(mode==1){
         		setXY(event);
 
         	}
         });
         scene.setOnMouseDragged((event)->{
         	if(mode==0) {
-        		canvas.toFront(); //念のために前面に
+        		canvas.toFront(); //描画キャンパスを前面に
         		paint(event,gc);
         	}
         	else if(mode==2) {
-        		canvas2.toFront();
+        		canvas2.toFront();//予測線描画キャンパスを前面に
         		square(event,gc2);
         	}
         	else if(mode==1){
-        		canvas2.toFront();
+        		canvas2.toFront();//予測線描画キャンパスを前面に
         		line2(event,gc2);
         	}
         });
@@ -144,15 +145,11 @@ public class Window extends Application {
         		canvas2.toFront();
         		setNodes(event,pane,canvas,canvas2,gc,gc2);
         		modechange(4);
-        		//canvas2.toFront();
         	}
 
         });
-
         root.setTop(menuBar);
-
         root.setCenter(pane);
-
         stage.setScene(scene);
         stage.show();
 
@@ -189,7 +186,16 @@ public class Window extends Application {
 		gc.strokeLine(x1,y,x1,y1);
 		gc.strokeLine(x,y1,x1,y1);
 	}
-
+	private void hsquare(MouseEvent event, GraphicsContext gc, int X, int Y) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x1 = (int)event.getX();
+		int y1 = (int)event.getY();
+		gc.clearRect(0, 0, 800, 600);
+		gc.strokeLine(x,y1,X,y1);
+		gc.strokeLine(x,Y,x,y1);
+		gc.strokeLine(X,y1,X,Y);
+		gc.strokeLine(x,Y,X,Y);
+	}
 	public void setNodes(MouseEvent event, Pane pane,Canvas canvas, Canvas canvas2, GraphicsContext gc, GraphicsContext gc2) {
 		int x1 = (int)event.getX();
 		int y1 = (int)event.getY();
@@ -203,36 +209,75 @@ public class Window extends Application {
 		Circle circle6 = new Circle(x,(y+y1)/2,5);
 		Circle circle7 = new Circle(x1,(y+y1)/2,5);
 		Circle circle8 = new Circle((x+x1)/2,y1,5);
-		circle.setOnMouseClicked((e) -> {
-			System.out.print("c1");
-		});
         circle.setOnMouseDragged((e)->{
         		setXY(x1,y1);
         		square(e,gc2);
         });
+        circle.setOnMouseEntered((e)->{
+        	circle.setCursor( Cursor.HAND );
+        });
         circle.setOnMouseReleased((e)->{
 
-        	big(e,gc,canvas,origx,origy,x1,y1,pane);
+        	big(e,gc,canvas,x,y,x1,y1,(int)e.getX(),(int)e.getY(),pane);
         	pane.getChildren().removeAll(circle,circle2,circle3,circle4,circle5,circle6,circle7,circle8);
         	gc2.clearRect(0, 0, 800, 600);
         	modechange(0);
         });
         circle.toFront();
 
-		circle2.setOnMouseClicked((e) -> {
-			System.out.print("c1");
-		});
+        circle2.setOnMouseEntered((e)->{
+        	circle2.setCursor( Cursor.HAND );
+        });
         circle2.setOnMouseDragged((e)->{
-        		setXY(x,y1);
-        		square(e,gc2);
+    		setXY(x,y1);
+    		square(e,gc2);
+    });
+        circle2.setOnMouseReleased((e)->{
+
+        	big(e,gc,canvas,x,origy,x1,y1,(int)e.getX(),(int)e.getY(),pane);
+        	pane.getChildren().removeAll(circle,circle2,circle3,circle4,circle5,circle6,circle7,circle8);
+        	gc2.clearRect(0, 0, 800, 600);
+        	modechange(0);
         });
         circle2.toFront();
 
+        circle3.setOnMouseEntered((e)->{
+        	circle3.setCursor( Cursor.HAND );
+        });
+        circle4.setOnMouseEntered((e)->{
+        	circle4.setCursor( Cursor.HAND );
+        });
 
+        circle5.setOnMouseDragged((e)->{
+    		hsquare(e,gc2,x1,y1);
+        });
+        circle5.setOnMouseReleased((e)->{
+
+        	big(e,gc,canvas,x,y,x1,y1,x,(int)e.getY(), pane);
+        	pane.getChildren().removeAll(circle,circle2,circle3,circle4,circle5,circle6,circle7,circle8);
+        	gc2.clearRect(0, 0, 800, 600);
+        	modechange(0);
+        });
+        circle5.setOnMouseEntered((e)->{
+        	circle5.setCursor( Cursor.HAND );
+        });
+        circle6.setOnMouseEntered((e)->{
+        	circle6.setCursor( Cursor.HAND );
+        });
+        circle7.setOnMouseEntered((e)->{
+        	circle7.setCursor( Cursor.HAND );
+        });
+        circle8.setOnMouseEntered((e)->{
+        	circle8.setCursor( Cursor.HAND );
+        });
 
         pane.getChildren().addAll();
 		pane.getChildren().addAll(circle,circle2,circle3,circle4,circle5,circle6,circle7,circle8);
 	}
+
+
+
+
 
 	public void setXY(MouseEvent e) {
 		x = (int)e.getX();
@@ -242,22 +287,24 @@ public class Window extends Application {
 		x = x1;
 		y= y1;
 	}
-	public void big(MouseEvent event,GraphicsContext gc,Canvas canvas, int origx, int origy,int X, int Y, Pane pane){
-		int x1 = (int)event.getX();
-		int y1 = (int)event.getY();
+	public void big(MouseEvent event,GraphicsContext gc,Canvas canvas, int origx, int origy,int X, int Y, int x1, int y1, Pane pane){
+		//int x1 = (int)event.getX();
+		//int y1 = (int)event.getY();
 		Image image = canvas.snapshot(null, null);
+		//選択範囲をwritableimageに取り込む
 		WritableImage resizedImage = new WritableImage(image.getPixelReader(),
 		        origx,origy,X-origx,Y-origy);
-		WritableImage voidImage = new WritableImage(X-origx,Y-origy);
+		WritableImage voidImage = new WritableImage(X-origx,Y-origy);//今まで描画されてた部分に上書きする用の画像
 		PixelWriter pw = voidImage.getPixelWriter();
 
+		//真っ白い画像をつくる
 		for(int i=0;i<X-origx;i++) {
 			for(int j=0;j<Y-origy;j++) {
 				pw.setColor(i, j, Color.WHITE);
 			}
 		}
-		gc.drawImage(voidImage, origx, origy);
-		gc.drawImage(resizedImage,x1,y1,X-x1,Y-y1);
+		gc.drawImage(voidImage, origx, origy); //前の部分を白で上書き
+		gc.drawImage(resizedImage,x1,y1,X-x1,Y-y1);//サイズを変更して貼り付け
 	}
     public static void modechange(int i) {
         mode=i;
